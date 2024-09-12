5c848230cd5352b82828e6ead308a74c03fe59f2
Ricky Wijaya <RJ23-2@slc.net>
2024-09-12 12:38:16 +0700

    fix: face recognition

--- face-recognition/app.py
+++ face-recognition/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from deepface import DeepFace
 import os
-from utils import decode_image, save_image
+from utils import decode_image, save_image, save_temp_image
 
 app = Flask(__name__)
 CORS(app)
@@ -11,32 +11,34 @@ STORAGE_PATH = 'storage'
 
 @app.route('/check-face', methods=['POST'])
 def check_face():
-  data = request.get_json()
-  image_base64 = data.get('image')
-  image = decode_image(image_base64)
-
   try:
+    data = request.get_json()
+    image_base64 = data.get('image')
+    image_array = decode_image(image_base64)
+    temp_image_path = save_temp_image(image_array)
+
     for file_name in os.listdir(STORAGE_PATH):
       file_path = os.path.join(STORAGE_PATH, file_name)
             
       result = DeepFace.verify(
-        img1_path=image, 
-        img2_path=file_path, 
-        model_name='VGG-Face',
-        detector_backend='retinaface',
-        anti_spoofing=True
+        img1_path=temp_image_path, 
+        img2_path=file_path
       )
-      print(f"Comparing with {file_name}: {result}")
+      print(f"Comparing with {file_name}: {result['distance']}")
             
-      if result['verified'] == True:
-        save_image(image, STORAGE_PATH)
+      if result['distance'] < 0.4:
         return jsonify({"message": f"Match found with {file_name}!"}), 200
         
+    save_image(image_array, STORAGE_PATH)
     return jsonify({"message": "No match found."}), 200
     
   except Exception as e:
     print(f"Error: {e}")
-    return jsonify({"message": "Error processing image."}), 500
+    return jsonify({"message": {e}}), 500
+  
+  finally:
+    if os.path.exists(temp_image_path):
+      os.remove(temp_image_path)
 
 if __name__ == '__main__':
   app.run(port=5000)
--- face-recognition/utils.py
+++ face-recognition/utils.py
@@ -4,6 +4,9 @@ import os
 from io import BytesIO
 from PIL import Image
 from datetime import datetime
+import time
+
+TEMP_PATH = 'temp'
 
 def decode_image(image_data):
   image_data = image_data.split(",")[1]
@@ -11,12 +14,20 @@ def decode_image(image_data):
   image = Image.open(BytesIO(image_bytes))
   return np.array(image)
 
-def save_image(image, folder):
+def save_temp_image(image_array):
+  file_name = f"{TEMP_PATH}/temp_image_{int(time.time())}.jpg"
+    
+  temp_image = Image.fromarray(image_array)
+  temp_image.save(file_name)
+
+  return file_name
+
+def save_image(image_array, folder):
     # Generate a timestamp-based file name
     timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
     file_name = f"{timestamp}.jpg"
     file_path = os.path.join(folder, file_name)
     
     # Save the image
+    image = Image.fromarray(image_array)
     image.save(file_path)
-    return file_path
